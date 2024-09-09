1a5de068e324e624977f66cfe2b1fa2182a10db7
Krishna <krishnasb2005@gmail.com>
2024-09-09 22:58:54 +0530

    addes database

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() =>{
-    await mongoose.connect('mongodb+srv://krishnasb2005:krish2005@cluster0.fatll.mongodb.net/food-delivery').then(()=> console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_URI).then(()=> console.log("DB Connected"));
 }
